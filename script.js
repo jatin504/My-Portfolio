54daa71bb7b208fc38a094029178eee06e99d11d
jatin504 <jt872678@gmail.com>
2024-07-27 17:51:25 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const sidemenu = document.querySelector('#sidemenu');
+
+function openmenu(){
+    sidemenu.style.transform = 'translateX(-16rem)';
+}
+
+function closemenu(){
+    sidemenu.style.transform = 'translateX(16rem)';
+}
