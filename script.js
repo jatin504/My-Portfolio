ba19aea5224d7c8949c70a6264dc3c65987f7cf0
jatin504 <jt872678@gmail.com>
2024-07-28 17:59:05 +0530

    creating a sections

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const sidemenu = document.querySelector('#sidemenu');
+const nav = document.querySelector("nav")
+const navlinks = document.querySelector("nav ul")
 
 function openmenu(){
     sidemenu.style.transform = 'translateX(-16rem)';
@@ -7,3 +9,13 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.transform = 'translateX(16rem)';
 }
+
+window.addEventListener('scroll',()=>{
+    if(scrollY > 50){
+        nav.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', "shadow-sm");
+        navlinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50')
+    }else{
+        nav.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', "shadow-sm")
+        navlinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50')
+    }
+})
